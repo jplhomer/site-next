76c0066b5a0a67562df48755aa238f5d08bb5d45
Josh Larson <jplhomer@gmail.com>
2020-05-18 08:46:04 -0500

    Get archive posts pulled into index

---
+++ pages/[...slug].js
@@ -0,0 +1,44 @@
+import { useRouter } from "next/router";
+import Head from "next/head";
+import { getArchivePosts } from "@/lib/archive";
+
+export async function getStaticProps({ params }) {
+  const res = await fetch(
+    `http://jplhomer.test/wp-json/wp/v2/posts?slug=${params.slug[2]}`
+  );
+  const post = await res.json();
+
+  return {
+    props: { post: post[0] },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = await getArchivePosts();
+  const paths = posts.map((post) => ({
+    params: {
+      slug: post.nextSlug,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function ArchivePost({ post }) {
+  const router = useRouter();
+
+  if (router.isFallback) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <Head>
+        <title>{post.title.rendered}</title>
+      </Head>
+      <h1>{post.title.rendered}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,42 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
+import { getArchivePosts } from "@/lib/archive";
 
-export default function Home() {
+export async function getStaticProps() {
+  let archivePosts = await getArchivePosts();
+
+  return {
+    props: {
+      archivePosts: archivePosts.slice(0, 4),
+    },
+  };
+}
+
+export default function Home({ archivePosts }) {
   return (
     <div className="container mx-auto p-4">
       <Head>
         <title>Josh Larson</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <h1 className="text-4xl font-black mb-4">Josh Larson</h1>
-
-      - Software engineer
-      - Work at Vox Media
-      - Passionate about solving hard problems and creating good developer experiences
-
-      WRITING
-      - Main blog (MDX posts from here)
-      - Inside Rafter
-      - Building Barkpass
-      - Archive (WP)
-
-      PROJECTS
-      - Barkpass
-      - Rafter
-      - Fresa
-      - Full Stack Fundamentals (on-hold)
-      - Lifeboat (archived)
-
-      GLANCES
-      - Videos with Barrett
-      - Good pics I want to share
-      - Livestreams
-      - Other quirky things
+      <ul>
+        {archivePosts.map((post) => {
+          return (
+            <li key={post.id}>
+              <Link href="/[...slug]" as={`/${post.nextSlug.join("/")}`}>
+                <a>{post.title.rendered}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      - Software engineer - Work at Vox Media - Passionate about solving hard
+      problems and creating good developer experiences WRITING - Main blog (MDX
+      posts from here) - Inside Rafter - Building Barkpass - Archive (WP)
+      PROJECTS - Barkpass - Rafter - Fresa - Full Stack Fundamentals (on-hold) -
+      Lifeboat (archived) GLANCES - Videos with Barrett - Good pics I want to
+      share - Livestreams - Other quirky things
     </div>
-  )
+  );
 }
