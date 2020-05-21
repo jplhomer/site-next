53a9530446f01e857031214ce95d4304a50d8306
Josh Larson <jplhomer@gmail.com>
2020-05-20 22:32:20 -0500

    Properly get static paths

---
+++ components/Loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="h-screen flex">
+      <p className="m-auto text-xl text-gray-600">Loading...</p>
+    </div>
+  );
+}
--- pages/archives/[page].js
+++ pages/archives/[page].js
@@ -1,24 +1,30 @@
 import ArchivePosts from '@/components/ArchivePosts';
 import { getArchivePosts } from '@/lib/archive-posts';
 import { PER_PAGE } from '../archives';
+import { useRouter } from 'next/router';
+import Loading from '@/components/Loading';
 
 export async function getStaticPaths() {
-  const { total } = getArchivePosts(1);
-  const totalPages = Math.ceil(total / PER_PAGE);
-
+  const paths = [
+    {
+      params: { page: '1' },
+    },
+    {
+      params: { page: '2' },
+    },
+    {
+      params: { page: '3' },
+    },
+  ];
   return {
-    paths: Array.from({ length: totalPages }, (v, i) => i).map((p) => ({
-      params: {
-        page: p,
-      },
-    })),
-    fallback: false,
+    paths,
+    fallback: true,
   };
 }
 
 export async function getStaticProps({ params }) {
   const { page } = params;
-  const { posts, total } = await getArchivePosts(PER_PAGE, PER_PAGE * (page - 1));
+  const { posts, total } = await getArchivePosts(PER_PAGE, PER_PAGE * (Number(page) - 1));
 
   return {
     props: {
@@ -30,5 +36,11 @@ export async function getStaticProps({ params }) {
 }
 
 export default function ArchivePage(props) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loading />;
+  }
+
   return <ArchivePosts {...props} />;
 }
