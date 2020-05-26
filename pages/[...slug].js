8766b5761d70640fd204dd777717dbdcb0c6bafd
Josh Larson <jplhomer@gmail.com>
2020-05-26 08:25:26 -0500

    Give some TLC to old archive posts

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -29,11 +29,21 @@ export default function ArchivePost({ post }) {
 
   if (router.isFallback) return <p>Loading...</p>;
 
+  const openGraphImages = post.thumbnail ? [{ url: post.thumbnail }] : [];
+
   return (
     <div>
-      <NextSeo title={post.title} />
+      <NextSeo
+        title={post.title}
+        openGraph={{
+          images: openGraphImages,
+        }}
+      />
       <div className="max-w-3xl p-4 mx-auto prose">
         <h1 className="mb-4">{post.title}</h1>
+        {post.dek && (
+          <div className="text-xl mb-4 text-gray-600 italic" dangerouslySetInnerHTML={{ __html: post.dek }} />
+        )}
         <div className="mb-8">
           <time dateTime={new Date(post.date).toISOString()}>{new Date(post.date).toLocaleDateString()}</time>
         </div>
