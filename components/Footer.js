9b5374eecf3b479c4218cb2bdc28145c1edf55cc
Josh Larson <jplhomer@gmail.com>
2020-05-30 07:13:06 -0500

    Move footer to a component

---
+++ components/Footer.js
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+
+export default function Footer() {
+  return (
+    <footer className="mt-12 py-8 px-4 max-w-4xl mx-auto text-center">
+      <img
+        className="rounded-full mb-4 w-20 h-20 mx-auto"
+        src="https://unavatar.now.sh/jplhomer"
+        alt="A photo of Josh"
+      />
+      <div className="mb-4">
+        <Link href="/">
+          <a className="font-bold text-lg">Josh Larson</a>
+        </Link>
+      </div>
+      <div className="mb-4">
+        <a className="mx-2" href="https://twitter.com/jplhomer">
+          Twitter
+        </a>
+        <a className="mx-2" href="https://github.com/jplhomer">
+          GitHub
+        </a>
+        <a className="mx-2" href="https://linkedin.com/in/jplhomer">
+          LinkedIn
+        </a>
+        <a className="mx-2" href="https://instagram.com/jplhomer">
+          Instagram
+        </a>
+        <a className="mx-2" href="mailto:jplhomer@gmail.com">
+          Email
+        </a>
+      </div>
+      <div className="text-xs">
+        <Link href="/archives">
+          <a className="mx-2">Archives</a>
+        </Link>
+        <a className="mx-2" href="/rss-feed.xml">
+          RSS
+        </a>
+        <a className="mx-2" href="https://eepurl.com/c0jXpP">
+          Newsletter
+        </a>
+      </div>
+    </footer>
+  );
+}
--- components/Layout.js
+++ components/Layout.js
@@ -1,51 +1,12 @@
-import Link from 'next/link';
 import Header from './Header';
+import Footer from './Footer';
 
 export default function Layout({ children }) {
   return (
     <div className="border-t-8 border-blue-800 dark:border-blue-300 dark:bg-gray-900 dark:text-gray-100">
       <Header />
       <main>{children}</main>
-      <footer className="mt-12 py-8 px-4 max-w-4xl mx-auto text-center">
-        <img
-          className="rounded-full mb-4 w-20 h-20 mx-auto"
-          src="https://unavatar.now.sh/jplhomer"
-          alt="A photo of Josh"
-        />
-        <div className="mb-4">
-          <Link href="/">
-            <a className="font-bold text-lg">Josh Larson</a>
-          </Link>
-        </div>
-        <div className="mb-4">
-          <a className="mx-2" href="https://twitter.com/jplhomer">
-            Twitter
-          </a>
-          <a className="mx-2" href="https://github.com/jplhomer">
-            GitHub
-          </a>
-          <a className="mx-2" href="https://linkedin.com/in/jplhomer">
-            LinkedIn
-          </a>
-          <a className="mx-2" href="https://instagram.com/jplhomer">
-            Instagram
-          </a>
-          <a className="mx-2" href="mailto:jplhomer@gmail.com">
-            Email
-          </a>
-        </div>
-        <div className="text-xs">
-          <Link href="/archives">
-            <a className="mx-2">Archives</a>
-          </Link>
-          <a className="mx-2" href="/rss-feed.xml">
-            RSS
-          </a>
-          <a className="mx-2" href="https://eepurl.com/c0jXpP">
-            Newsletter
-          </a>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
