c2c5196cf76895c809694783eec1fe177c5af89a
Josh Larson <jplhomer@gmail.com>
2020-05-30 07:53:29 -0500

    Fix link to noflash

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head />
         <body>
-          <script src="noflash.js" />
+          <script src="/noflash.js" />
           <Main />
           <NextScript />
         </body>
