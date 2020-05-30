990ca0e4e9e80ac1dec5100dbdc858995f10053a
Josh Larson <jplhomer@gmail.com>
2020-05-30 08:15:18 -0500

    Add link to archives on posts

--- pages/posts.js
+++ pages/posts.js
@@ -3,6 +3,7 @@ import { getPosts } from '@/lib/posts';
 import { NextSeo } from 'next-seo';
 import PostListItem from '@/components/PostListItem';
 import Heading from '@/components/Heading';
+import Link from 'next/link';
 
 export async function getStaticProps() {
   const posts = await getPosts();
@@ -20,11 +21,19 @@ export default function Posts({ posts }) {
       <NextSeo title="Posts" />
       <Heading className="mb-8">Posts</Heading>
 
-      <ul>
+      <ul className="mb-8">
         {posts.map((post) => {
           return <PostListItem key={post.path} post={post} href="/posts/[slug]" as={`/posts/${post.nextPath}`} />;
         })}
       </ul>
+
+      <p className="italic">
+        Looking for older stuff?{' '}
+        <Link href="/archives">
+          <a className="underline">Check out the archives</a>
+        </Link>
+        .
+      </p>
     </Wrapper>
   );
 }
