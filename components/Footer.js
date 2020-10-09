c502e30641f223e8008804259d0d670e962e6c59
Josh Larson <jplhomer@gmail.com>
2020-10-09 11:02:26 -0500

    Use actual headshot

--- components/Footer.js
+++ components/Footer.js
@@ -3,11 +3,7 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer className="mt-12 py-8 px-4 max-w-4xl mx-auto text-center">
-      <img
-        className="rounded-full mb-4 w-20 h-20 mx-auto"
-        src="https://unavatar.now.sh/jplhomer"
-        alt="A photo of Josh"
-      />
+      <img className="rounded-full mb-4 w-20 h-20 mx-auto" src="/headshot.jpg" alt="A photo of Josh" />
       <div className="mb-4">
         <Link href="/">
           <a className="font-bold text-lg">Josh Larson</a>
