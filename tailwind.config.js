e50205d5ea227d9d25c6f269f0e4fc4d3d6db5bc
Josh Larson <jplhomer@gmail.com>
2021-06-20 21:07:46 -0500

    Fix dark mode prose styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,42 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: {
-    content: ['./components/**/*.js', './pages/**/*.js'],
-    options: {
-      whitelist: ['dark-mode'],
-    },
-  },
+  purge: ['./components/**/*.js', './pages/**/*.js'],
   mode: 'jit',
   darkMode: 'class',
   theme: {
+    typography: (theme) => ({
+      default: {
+        css: {
+          color: theme('colors.gray.900'),
+
+          a: {
+            color: theme('colors.blue.500'),
+            '&:hover': {
+              color: theme('colors.blue.700'),
+            },
+          },
+        },
+      },
+
+      dark: {
+        css: {
+          color: theme('colors.gray.100'),
+
+          a: {
+            color: theme('colors.blue.100'),
+            '&:hover': {
+              color: theme('colors.blue.100'),
+            },
+          },
+        },
+      },
+    }),
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
-    darkSelector: '.dark-mode',
   },
   plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/typography')],
 };
